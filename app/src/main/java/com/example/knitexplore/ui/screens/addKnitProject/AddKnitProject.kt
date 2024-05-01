b6dd53f58e70313b15a782a0cfcd297a1cab6db2
ElinSimonsson <elin.simonsson@iths.se>
2024-05-01 20:28:48 +0200

    updated ui in addKnitProjectScreen

--- app/src/main/java/com/example/knitexplore/ui/screens/addKnitProject/AddKnitProject.kt
+++ app/src/main/java/com/example/knitexplore/ui/screens/addKnitProject/AddKnitProject.kt
@@ -3,6 +3,7 @@ package com.example.knitexplore.ui.screens.addKnitProject
 
 import android.net.Uri
 import android.util.Log
+import android.view.MotionEvent
 import android.widget.ScrollView
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.PickVisualMediaRequest
@@ -16,6 +17,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -23,18 +25,27 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -42,22 +53,31 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.platform.LocalView
 
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 import com.example.knitexplore.R
+import kotlinx.coroutines.launch
 
 @Composable
 fun AddKnitProject () {
 
 
     val viewModel : AddKnitProjectViewController = viewModel()
+    val listState = rememberLazyListState()
 
     val singlePhotoPicker = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.PickVisualMedia(),
@@ -69,7 +89,12 @@ fun AddKnitProject () {
         }
     )
 
+    val coroutineScope = rememberCoroutineScope()
+
+
+
     LazyColumn(
+        state = listState,
         modifier= Modifier
             .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -99,59 +124,120 @@ fun AddKnitProject () {
                 }
             }
         }
-        item {
+        /*item {
             SubTitle("Name your project")
+        }*/
+        item {
+            TextInput(viewModel = viewModel, "projectName", label = "Enter your project")
+        }
+        /*item {
+            SubTitle("Enter pattern name")
+        }*/
+        item {
+            TextInput(viewModel = viewModel, fieldName = "pattern", label = "Enter pattern name")
+        }
+
+        item {
+            repeat(viewModel.numberOfNeedleSizes) {
+                TextInput(viewModel = viewModel, fieldName = "needleSize", label = "Needle size" )
+            }
+        }
+
+        item {
+          AddNeedleBtn(viewModel = viewModel) {
+              /*coroutineScope.launch {
+                  listState.scrollToItem(index = viewModel.numberOfNeedleSizes, scrollOffset = 50)
+              }*/
+          }
         }
         item {
-            ProjectNameInput(viewModel = AddKnitProjectViewController())
+            SaveBtn(viewModel = viewModel)
         }
         item {
-            SaveBtn()
+            Spacer(modifier = Modifier.height(50.dp))
         }
-        item {
-            SubTitle("Name your project")
+    }
+}
+
+
+@Composable
+fun AddNeedleBtn (viewModel: AddKnitProjectViewController,scrollScreenDown: () -> Unit) {
+    Row (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(start = 20.dp, top = 10.dp)
+
+
+    ) {
+        Box(modifier = Modifier.clickable {
+            scrollScreenDown()
+            viewModel.addNeedleSize()
+        }) {
+            Row {
+                Icon(
+                    imageVector = Icons.Default.AddCircle,
+                    contentDescription = "Add",
+                    tint = Color(0xFFA3D9A5))
+                Text(
+                    text = "Add needle",
+                    modifier = Modifier.padding(start = 10.dp))
+            }
         }
     }
 }
 
 @Composable
-fun SaveBtn () {
+fun SaveBtn (viewModel: AddKnitProjectViewController) {
+
     Row {
-        Button(onClick = { Log.d("!!!", "want to save") })
+        Button(onClick = { viewModel.trySave() })
         {
             Text(text = "Save")
         }
-
     }
 }
 
 @Composable
-fun SubTitle (text: String) {
-    Row (
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 20.dp)
-    ) {
-        Text(text = text)
-    }
-}
+fun TextInput (viewModel: AddKnitProjectViewController, fieldName: String, label : String) {
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun ProjectNameInput (viewModel: AddKnitProjectViewController) {
-    var projectName by remember { mutableStateOf(viewModel.projectName) }
+    val focusManager = LocalFocusManager.current
+    var text by remember { mutableStateOf("") }
+    var keyboardType: KeyboardType = KeyboardType.Text
+
+
+   when (fieldName) {
+        "projectName" -> text = viewModel.projectName
+        "pattern" -> text = viewModel.patternName
+        "needleSize" -> {
+           text = viewModel.needleSize?.toString() ?: ""
+           keyboardType = KeyboardType.Number
+       }
+
+   }
 
     Row {
-
         TextField(
-            value = projectName,
+            value = text,
             onValueChange = { newText ->
-                projectName = newText
-                viewModel.projectName = newText
+
+                when (fieldName) {
+                    "projectName" -> viewModel.updateProjectText(newText)
+                    "pattern" -> viewModel.updatePatternName(newText)
+                    "needleSize" -> viewModel.updateNeedleSize(newText.toDouble())
+                }
+            },
+            label = {
+                    Text(label)
             },
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(horizontal = 20.dp),
+                .padding(horizontal = 20.dp, vertical = 5.dp),
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done, keyboardType = keyboardType),
+            keyboardActions = KeyboardActions(
+                onDone =  {
+                    focusManager.clearFocus()
+                }
+            ),
             colors = TextFieldDefaults.colors(
                 focusedContainerColor = Color.Transparent,
                 unfocusedContainerColor = Color.Transparent,
@@ -188,7 +274,7 @@ fun SelectedImage (uri: Uri, onClickAction: () -> Unit) {
         contentDescription = null,
         contentScale = ContentScale.Crop,
         modifier = Modifier
-            .size(300.dp)
+            .size(350.dp)
             .padding(20.dp)
             .clip(RoundedCornerShape(10.dp))
             .clickable {
@@ -210,6 +296,4 @@ fun Title () {
             )
         )
     }
-
-
 }
--- app/src/main/java/com/example/knitexplore/ui/screens/addKnitProject/AddKnitProjectViewController.kt
+++ app/src/main/java/com/example/knitexplore/ui/screens/addKnitProject/AddKnitProjectViewController.kt
@@ -3,6 +3,8 @@ package com.example.knitexplore.ui.screens.addKnitProject
 import android.net.Uri
 import android.util.Log
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableDoubleStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -16,10 +18,33 @@ import java.util.Locale
 class AddKnitProjectViewController : ViewModel() {
     var imageUriState by  mutableStateOf<Uri?>(null)
     var projectName by mutableStateOf("")
+    var patternName by mutableStateOf("")
+    var needleSize by mutableStateOf<Double?>(null)
+    var numberOfNeedleSizes by mutableIntStateOf(1)
+
     private var storageRef = Firebase.storage.reference
+
+    fun addNeedleSize() {
+        numberOfNeedleSizes++
+    }
 
     fun setImageUri (uri : Uri) {
         imageUriState = uri
+    }
+
+    fun trySave () {
+        Log.d("!!!", "projectName: $projectName patternName: $patternName  needleSize: $needleSize")
+    }
+
+    fun updateNeedleSize (number: Double) {
+        needleSize = number
+    }
+    fun updateProjectText (text: String) {
+        projectName = text
+    }
+
+    fun updatePatternName (text: String) {
+        patternName = text
     }
 
     fun saveImageToStorage() {
